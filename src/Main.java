bae3518253e7ee9d5f6b34446f9f0916ea87a646
Fernando Trujillo <fernando.trujillo@Fernandos-MacBook-Air.local>
2023-10-22 00:04:42 -0500

    first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+// First Java Program - Road To SE
+
+//Defining Class
+class HelloWorld {
+    //Required main method + varargs parameters
+    public static void main (String[] args){
+        //Outputs String to console
+        System.out.println("Hello World!");
+    }
+}
